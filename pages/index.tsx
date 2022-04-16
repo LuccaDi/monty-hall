c74c4557425cbb636c47ef26c320fdb1872ca78d
LuccaDi <lucca_di@hotmail.com>
2022-04-16 00:30:46 -0300

    Developed forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,43 @@
+import styles from "../styles/Formulario.module.css";
+import Card from "../components/Card";
+import Link from "next/link";
+import EntradaNumerica from "../components/EntradaNumerica";
 import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
-import PortaModel from "../model/porta";
 
-export default function Home() {
+export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3);
+  const [comPresente, setComPresente] = useState(1);
+
   return (
-    <div style={{ display: "flex" }}>
-      <h1>Formulários de Início!!!</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card>
+          <EntradaNumerica
+            text="Qtde Portas?"
+            value={qtdePortas}
+            onChange={(novaQtde) => setQtdePortas(novaQtde)}
+          />
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <EntradaNumerica
+            text="Porta com Presente?"
+            value={comPresente}
+            onChange={(novaPortaComPresente) =>
+              setComPresente(novaPortaComPresente)
+            }
+          />
+        </Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
