b487598e9a546e87ddefc167d09d60e2b8800248
LuccaDi <lucca_di@hotmail.com>
2022-04-15 22:38:40 -0300

    Developed game component

--- pages/jogo.tsx
+++ pages/jogo.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/Jogo.module.css";
 import { useState } from "react";
 import Porta from "../components/Porta";
 import { atualizarPortas, criarPortas } from "../functions/portas";
+import Link from "next/link";
 
 export default function jogo() {
   const [portas, setPortas] = useState(criarPortas(14, 3));
@@ -22,8 +23,12 @@ export default function jogo() {
 
   return (
     <div id={styles.jogo}>
-      <div className={styles.portas}>{renderizarPortas()}</div>;
-      <div className={styles.botoes}></div>
+      <div className={styles.portas}>{renderizarPortas()}</div>
+      <div className={styles.botoes}>
+        <Link href="/">
+          <button>Reiniciar Jogo</button>
+        </Link>
+      </div>
     </div>
   );
 }
