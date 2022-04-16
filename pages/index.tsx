90d3e122b2cd3352d60f0df94ed2a5a141251788
LuccaDi <lucca_di@hotmail.com>
2022-04-15 22:17:32 -0300

    Developed game component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,9 @@ import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(4, 3));
-
-  function renderizarPortas() {
-    return portas.map((porta) => {
-      return (
-        <Porta
-          key={porta.numero}
-          value={porta}
-          onChange={(novaPorta) =>
-            setPortas(atualizarPortas(portas, novaPorta))
-          }
-        />
-      );
-    });
-  }
-
-  return <div style={{ display: "flex" }}>{renderizarPortas()}</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <h1>Formulários de Início!!!</h1>
+    </div>
+  );
 }
